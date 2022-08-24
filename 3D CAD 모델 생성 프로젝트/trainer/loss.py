5793ab7a1ebdf5a8e012041c66da6b4d9ac8151b
john980302 <40467774+john980302@users.noreply.github.com>
2022-08-24 17:49:52 +0900

    3D CAD 모델 생성 프로젝트

---
+++ 3D CAD 모델 생성 프로젝트/trainer/loss.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from model.model_utils import _get_padding_mask, _get_visibility_mask
+from cadlib.macro import CMD_ARGS_MASK
+
+
+class CADLoss(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.n_commands = cfg.n_commands
+        self.args_dim = cfg.args_dim + 1
+        self.weights = cfg.loss_weights
+        
+        self.beta_end = cfg.beta_end
+        self.beta_start = cfg.beta_start
+        self.beta_rate = cfg.beta_rate
+        
+        self.use_VAE = cfg.use_VAE
+
+        self.register_buffer("cmd_args_mask", torch.tensor(CMD_ARGS_MASK))
+
+    def forward(self, output, epoch):
+        # Target & predictions
+        tgt_commands, tgt_args = output["tgt_commands"], output["tgt_args"]
+
+        visibility_mask = _get_visibility_mask(tgt_commands, seq_dim=-1)
+        padding_mask = _get_padding_mask(tgt_commands, seq_dim=-1, extended=True) * visibility_mask.unsqueeze(-1)
+
+        command_logits, args_logits = output["command_logits"], output["args_logits"]
+
+        mask = self.cmd_args_mask[tgt_commands.long()]
+
+        loss_cmd = F.cross_entropy(command_logits[padding_mask.bool()].reshape(-1, self.n_commands), tgt_commands[padding_mask.bool()].reshape(-1).long())
+        loss_args = F.cross_entropy(args_logits[mask.bool()].reshape(-1, self.args_dim), tgt_args[mask.bool()].reshape(-1).long() + 1)  # shift due to -1 PAD_VAL
+
+        loss_cmd = self.weights["loss_cmd_weight"] * loss_cmd
+        loss_args = self.weights["loss_args_weight"] * loss_args
+        
+        if self.use_VAE:
+            beta = self.beta_end + (self.beta_start - self.beta_end)*(self.beta_rate)**epoch
+            loss_kld = output['KLD_loss'] * beta
+
+            res = {"loss_cmd": loss_cmd, "loss_args": loss_args, 'loss_kld': loss_kld}
+        
+        else:
+            res = {"loss_cmd": loss_cmd, "loss_args": loss_args}
+            
+        return res
